647536a282fd2e3737a9af2f53ec1948dab41fb5
Julien Marie <hi@producture.com>
2024-08-07 15:30:25 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Parallel.Mixfile do
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
+      deps: deps(),
       package: [
         maintainers: ["Maximilian Stroh"],
         licences: ["MIT"],
